eb426eff84ce41d9b4b841721427c9ae9083bf1e
danny-guterman <60140218+danny-guterman@users.noreply.github.com>
2020-01-21 17:14:57 +0100

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# this notebook will be used for Capstone 
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
